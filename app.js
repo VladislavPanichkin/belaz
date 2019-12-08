f468a9f4978f5c182a44ef52254829e4ee9567da
Vladislav <loverbikeebns@gmail.com>
2019-12-08 22:35:43 +0300

    первая страница

---
+++ app.js
@@ -0,0 +1,10 @@
+/* $(document).ready(function() {    
+    console.log( "ready!" );
+    $(".input-form").on("focus", function(){
+        $(".input-form").attr('id', '.hide-bg');
+        console.log('shot');
+    });
+     $(".input-form").on("blur", function(){
+        $(".input-form").attr('id', 'your-phone');
+    });
+}); */
